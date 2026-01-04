99ce45c4d472b31a117de2eba57b64b95087d381
hnjwnaio <htd0618@naver.com>
2026-01-04 16:24:26 +0900

    더미 데이터 넣기

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+-- 1번 데이터
+INSERT INTO book_review(title, author, content, rating, created_at) VALUES('불편한 편의점', '김호연', '따뜻한 힐링 소설이네요.', 5, NOW());
+
+-- 2번 데이터
+INSERT INTO book_review(title, author, content, rating, created_at) VALUES('슬램덩크', '이노우에', '왼손은 거들 뿐.', 5, NOW());
+
+-- 3번 데이터
+INSERT INTO book_review(title, author, content, rating, created_at) VALUES('자바의 정석', '남궁성', '기초부터 탄탄하게!', 4, NOW());
