f620de0996d9e52e7eb48af0622387c619e8edb2
hnjwnaio <htd0618@naver.com>
2026-01-04 16:22:26 +0900

    Delete 구현

--- src/main/java/com/example/BookReviewProject/controller/BookReviewController.java
+++ src/main/java/com/example/BookReviewProject/controller/BookReviewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,5 +79,18 @@ public class BookReviewController {
         }
 
         return "redirect:/reviews/" + bookReviewEntity.getId();
+    }
+
+    // 게시글 삭제
+    @PostMapping("/reviews/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        BookReview target = bookReviewRepository.findById(id).orElse(null);
+
+        if (target != null) {
+            bookReviewRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+
+        return "redirect:/reviews";
     }
 }
