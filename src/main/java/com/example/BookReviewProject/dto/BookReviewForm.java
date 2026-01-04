082f60d5ed7d53e385b078b80109c1b98053123a
hnjwnaio <htd0618@naver.com>
2026-01-04 15:50:00 +0900

    DTO 작성

--- src/main/java/com/example/BookReviewProject/dto/BookReviewForm.java
+++ src/main/java/com/example/BookReviewProject/dto/BookReviewForm.java
@@ -1,4 +1,21 @@
 package com.example.BookReviewProject.dto;
 
+import com.example.BookReviewProject.entity.BookReview;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Setter
+@ToString
 public class BookReviewForm {
+    private Long id;
+    private String title;
+    private String author;
+    private String content;
+    private Integer rating;
+
+    public BookReview toEntity() {
+        return new BookReview(null, title, author, content, rating, null);
+    }
 }
