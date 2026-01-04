78df0455b1efdb1066ae92f7c10fa14e5c8fd532
hnjwnaio <htd0618@naver.com>
2026-01-04 15:34:34 +0900

    first commit

---
+++ src/main/java/com/example/BookReviewProject/BookReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookReviewProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookReviewProject/BookReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookReviewProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
