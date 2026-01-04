aeb2010690e79e8db95e1bdfd1b807555a1150d4
hnjwnaio <htd0618@naver.com>
2026-01-04 15:47:54 +0900

    레포지토리 작성

--- src/main/java/com/example/BookReviewProject/repository/BookReviewRepository.java
+++ src/main/java/com/example/BookReviewProject/repository/BookReviewRepository.java
@@ -3,5 +3,9 @@ package com.example.BookReviewProject.repository;
 import com.example.BookReviewProject.entity.BookReview;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface BookReviewRepository extends CrudRepository<BookReview, Long> {
+    @Override
+    ArrayList<BookReview> findAll();
 }
